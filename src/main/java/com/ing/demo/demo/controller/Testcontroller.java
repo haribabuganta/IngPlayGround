4476f82688b991fd26ac8777553075d594b7fae9
Hari <haricg8@gmail.com>
2020-05-02 16:04:04 +0530

    initila commits

--- src/main/java/com/ing/demo/demo/controller/Testcontroller.java
+++ src/main/java/com/ing/demo/demo/controller/Testcontroller.java
@@ -2,6 +2,15 @@ package com.ing.demo.demo.controller;
 
 public class Testcontroller {
 
+
+    public static class Test{
+
+private int id;
+private String name;
+
+
+    }
+
     public static void main(String[] args) {
 
     }
