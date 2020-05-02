164b74b4691aa5a4a9351179877cb56e0f8af99a
Hari <haricg8@gmail.com>
2020-05-02 17:46:39 +0530

    test

--- src/main/java/com/ing/demo/demo/controller/Testcontroller.java
+++ src/main/java/com/ing/demo/demo/controller/Testcontroller.java
@@ -6,19 +6,6 @@ public class Testcontroller {
 
     private String name;
     private int id;
-
-    public Testcontroller(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
-
-    public int getCount() {
-
-        return 90;
-
-    }
-
-
     public String getDetails() {
 
         return "Hari";
@@ -36,8 +23,7 @@ private String name;
 
     public static void main(String[] args) {
         Testcontroller testcontroller = new Testcontroller();
-        int aa = testcontroller.getCount();
-        System.out.println("count------------" + aa);
+    
 
         Testcontroller testcontroller1=new Testcontroller();
        String ss= testcontroller1.getDetails();
