2935efbd53dc6e0832d3a3297fe72513ab0e6672
Hari <haricg8@gmail.com>
2020-05-02 15:57:00 +0530

    controller changes

--- src/main/java/com/ing/demo/demo/controller/Testcontroller.java
+++ src/main/java/com/ing/demo/demo/controller/Testcontroller.java
@@ -1,8 +1,28 @@
 package com.ing.demo.demo.controller;
 
 public class Testcontroller {
+    public Testcontroller() {
+    }
+
+    private String name;
+    private int id;
+
+    public Testcontroller(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getCount() {
+
+        return 90;
+
+    }
+
 
     public static void main(String[] args) {
+        Testcontroller testcontroller = new Testcontroller();
+        int aa = testcontroller.getCount();
+        System.out.println("count------------" + aa);
 
     }
 }
