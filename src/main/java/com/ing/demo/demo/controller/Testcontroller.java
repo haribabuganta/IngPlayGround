5aeb05815a4a03450bddd4d009919f3040a48f80
Hari <haricg8@gmail.com>
2020-05-02 15:50:54 +0530

    controller commit

--- src/main/java/com/ing/demo/demo/controller/Testcontroller.java
+++ src/main/java/com/ing/demo/demo/controller/Testcontroller.java
@@ -2,7 +2,17 @@ package com.ing.demo.demo.controller;
 
 public class Testcontroller {
 
+    public String getDetails() {
+
+        return "Hari";
+    }
+
+
     public static void main(String[] args) {
+
+        Testcontroller testcontroller=new Testcontroller();
+       String ss= testcontroller.getDetails();
+        System.out.println("details are "+ss);
 
     }
 }
