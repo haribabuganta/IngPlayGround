a4bca05f73c6ae0823d28ae7db61bafda6ee293b
Hari <haricg8@gmail.com>
2020-05-02 15:39:01 +0530

    2nd commit

---
+++ src/main/java/com/ing/demo/demo/controller/Testcontroller.java
@@ -0,0 +1,8 @@
+package com.ing.demo.demo.controller;
+
+public class Testcontroller {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/ing/demo/demo/service/TestService.java
@@ -0,0 +1,4 @@
+package com.ing.demo.demo.service;
+
+public class TestService {
+}
